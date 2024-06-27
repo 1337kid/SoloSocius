8555f56a86989620bd5818453ef2bb7a4b73b060
1337kid <45532566+1337kid@users.noreply.github.com>
2024-06-27 12:52:32 +0530

    feat: update profile page

--- src/app/api/public/getActor/route.js
+++ src/app/api/public/getActor/route.js
@@ -1,15 +1,22 @@
 import { connectToDB } from "@/db"
 import { getUserActorFromDB, getUserActorConnectionsCount } from "@/db/actor"
 import { NextResponse } from "next/server"
+import { INSTANCE } from "@/constants"
 
 export const GET = async () => {
     try {
         await connectToDB()
-        const userActor = await getUserActorFromDB("-password -fediverse -_id -__v");
-        if (!userActor) return NextResponse.json({eroor: 'account unavailable'}, {status:404})
+        const userActor_ = await getUserActorFromDB("-password -fediverse -_id -__v");
+        if (!userActor_) return NextResponse.json({eroor: 'account unavailable'}, {status:404})
         const connections = await getUserActorConnectionsCount();
+        const stats = [
+            {name: "Following", data: connections.following},
+            {name: "Followers", data: connections.followers}
+        ]
+        let userActor = userActor_._doc
+        userActor.username = `@${userActor.username}@${INSTANCE}`
         return NextResponse.json({
-            userActor, connections
+            userActor, stats
         }, {status:200});
     } catch (error) {
         console.log(error)
--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,12 @@ import { useRouter } from 'next/navigation';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { API_PUBLIC } from '@/constants/api';
+import Image from 'next/image';
+import DOMPurify from 'dompurify';
 
 const ActorProfile = () => {
   const router = useRouter();
-  const [data, setData] = useState({userActor: {}, connections: {}})
+  const [data, setData] = useState({userActor: {}, stats: []})
   useEffect(() => {
     const getUserActor = async () => {
       const data = await axios.get(`${API_PUBLIC}/getActor`)
@@ -16,9 +18,30 @@ const ActorProfile = () => {
     getUserActor();
   }, [])
   return (
-    <div className='flex container w-full bg-slate-300 m-auto'>
-      <div className='flex w-full'>
-        <h3>{data?.userActor?.name}</h3>
+    <div className='flex container w-full m-auto p-2 text-white'>
+      <div className='flex w-full gap-2'>
+        <div className='flex w-3/12 flex-col bg-zinc-900 p-2 rounded-md pt-4'>
+          <Image src={data?.userActor?.profilePhoto} alt="profile image" className='m-auto rounded-md' width={300} height={300}/>
+          <div className='flex flex-col p-2'>
+            <p className='text-lg'>{data?.userActor?.name}</p>
+            <p className='text-zinc-500 text-sm'>{data?.userActor?.username}</p>
+          </div>
+          <p className='p-2' dangerouslySetInnerHTML={{ __html:DOMPurify.sanitize(data?.userActor?.summary) }} />
+          <hr className='m-2 border-zinc-500'/>
+          <div className='flex gap-1 justify-around p-2'>
+            {data?.stats?.map((item) => (
+              <div key={item.name} className='text-zinc-300 text-center flex gap-1 items-center'>
+                {console.log('lmao')}
+                <p className='font-[600] text-zinc-200 text-md'>{item?.data}</p>
+                <p className='text-sm'>{item?.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='flex w-9/12 bg-zinc-900 p-2 rounded-md'> 
+          <h3 className='text-xl text-center w-full'>Notes</h3>
+
+        </div>
       </div>
     </div>
   )
